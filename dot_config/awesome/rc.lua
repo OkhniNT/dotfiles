a2ef5fb66347f8e66a031f2e660169a3bc1a6689
John <johnmichaelclasson@gmail.com>
2023-08-18 15:03:14 +1000

    Update .config/awesome/rc.lua

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -9,8 +9,11 @@ local naughty = require("naughty")
 local ruled = require("ruled")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
+local dpi = beautiful.xresources.apply_dpi
+
 require 'binds'
 require 'bar'
+require 'rules'
 
 require("awful.autofocus")
 
@@ -33,72 +36,15 @@ gears.timer {
 }
 -- }}}
 
-
 -- {{{ Variable definitions
 -- @DOC_LOAD_THEME@
--- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
--- beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 beautiful.init(gears.filesystem.get_xdg_cache_home() .. "wal/theme.lua")
 
--- {{{ Rules
--- Rules to apply to new clients.
--- @DOC_RULES@
-ruled.client.connect_signal("request::rules", function()
-    -- @DOC_GLOBAL_RULE@
-    -- All clients will match this rule.
-    ruled.client.append_rule {
-        id         = "global",
-        rule       = { },
-        properties = {
-            focus     = awful.client.focus.filter,
-            raise     = true,
-            screen    = awful.screen.preferred,
-            placement = awful.placement.no_overlap+awful.placement.no_offscreen
-        }
-    }
-
-    -- @DOC_FLOATING_RULE@
-    -- Floating clients.
-    ruled.client.append_rule {
-        id       = "floating",
-        rule_any = {
-            instance = { "copyq", "pinentry" },
-            class    = {
-                "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv",
-                "Tor Browser", "Wpa_gui", "veromix", "xtightvncviewer"
-            },
-            -- Note that the name property shown in xprop might be set slightly after creation of the client
-            -- and the name shown there might not match defined rules here.
-            name    = {
-                "Event Tester",  -- xev.
-            },
-            role    = {
-                "AlarmWindow",    -- Thunderbird's calendar.
-                "ConfigManager",  -- Thunderbird's about:config.
-                "pop-up",         -- e.g. Google Chrome's (detached) Developer Tools.
-            }
-        },
-        properties = { floating = true }
-    }
-
-    -- @DOC_DIALOG_RULE@
-    -- Add titlebars to normal clients and dialogs
-    ruled.client.append_rule {
-        -- @DOC_CSD_TITLEBARS@
-        id         = "titlebars",
-        rule_any   = { type = { "normal", "dialog" } },
-        properties = { titlebars_enabled = false      }
-    }
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- ruled.client.append_rule {
-    --     rule       = { class = "Firefox"     },
-    --     properties = { screen = 1, tag = "2" }
-    -- }
-end)
--- }}}
-
 -- {{{ Notifications
+
+naughty.config.defaults.timeout = 2
+naughty.config.defaults.border_width = dpi(4)
+naughty.config.defaults.margin = dpi(6)
 
 ruled.notification.connect_signal('request::rules', function()
     -- All notifications will match this rule.
@@ -106,7 +52,7 @@ ruled.notification.connect_signal('request::rules', function()
         rule       = { },
         properties = {
             screen           = awful.screen.preferred,
-            implicit_timeout = 5,
+            implicit_timeout = 2,
         }
     }
 end)
