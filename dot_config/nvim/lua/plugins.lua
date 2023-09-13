bd3982f4ebff033c49224ce9288a601117f7eea0
John <johnmichaelclasson@gmail.com>
2023-09-13 15:47:07 +1000

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -22,8 +22,18 @@ plugins = {
 
         configs.setup({
             ensure_installed = { 'c', 'python', 'lua', 'latex', 'markdown', 'markdown_inline', 'diff' },
-            highlight = { enable = true },
-            indent = { enable = true },
+            highlight = {
+                enable = true,
+                disable = { 'latex' },
+                disable = function (lang, buf)
+                    local max_filesize = 100 * 1024
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size < max_filesize then
+                        return true
+                    end
+                end
+            },
+            indent = { enable = false },
         })
     end },
 }
