88dfa0d04b41bb780f5b0b1daee2e9f146ae43c7
John <johnmichaelclasson@gmail.com>
2023-09-13 21:35:10 +1000

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -21,17 +21,10 @@ plugins = {
         local configs = require('nvim-treesitter.configs')
 
         configs.setup({
-            ensure_installed = { 'c', 'python', 'lua', 'latex', 'markdown', 'markdown_inline', 'diff' },
+            ensure_installed = { 'c', 'python', 'lua', 'markdown', 'markdown_inline', 'diff', 'latex' },
             highlight = {
                 enable = true,
                 disable = { 'latex' },
-                disable = function (lang, buf)
-                    local max_filesize = 100 * 1024
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size < max_filesize then
-                        return true
-                    end
-                end
             },
             indent = { enable = false },
         })
