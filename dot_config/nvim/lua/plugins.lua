6c44140493bbc567bd0849d5c02c11b4c94d37e9
John <johnmichaelclasson@gmail.com>
2023-09-16 02:09:01 +1000

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,26 +1,28 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 plugins = {
-    { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' , 'burntsushi/ripgrep' } },
-    { 'kylechui/nvim-surround', config = function () require('nvim-surround').setup() end },
-    { 'ms-jpq/coq-nvim', branch = 'coq' },
-    { 'rebelot/kanagawa.nvim' },
-    { 'cohama/lexima.vim' },
-    { 'nvim-treesitter/nvim-treesitter' , build = ':TSUpdate', config = function ()
-        local configs = require('nvim-treesitter.configs')
+    { 'rebelot/kanagawa.nvim', config = function ()
+        require('kanagawa').setup({
+            theme = 'dragon',
+            transparent = true,
+            colors = { theme = { all = { ui = { bg_gutter = 'none' } } } },
+        })
+        vim.cmd.colorscheme('kanagawa')
+    end },
 
-        configs.setup({
+    { 'nvim-treesitter/nvim-treesitter' , build = ':TSUpdate', config = function ()
+        require('nvim-treesitter.configs').setup({
             ensure_installed = { 'c', 'python', 'lua', 'markdown', 'markdown_inline', 'diff', 'latex' },
             highlight = {
                 enable = true,
@@ -28,7 +30,56 @@ plugins = {
             },
             indent = { enable = false },
         })
+
+        vim.opt.foldmethod = 'expr'
+        vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
     end },
+
+    { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' , 'burntsushi/ripgrep' }, config = function ()
+        local telescope = require('telescope.builtin')
+
+        vim.keymap.set('n', '<leader>tt', telescope.find_files, { noremap = true })
+        vim.keymap.set('n', '<leader>tg', telescope.live_grep, { noremap = true })
+        vim.keymap.set('n', '<leader>tb', telescope.buffers, { noremap = true })
+    end },
+
+    { 'ms-jpq/coq-nvim', branch = 'coq', config = function ()
+        vim.g.coq_settings = { ['keymap.jump_to_mark'] = '<c-a>', ['keymap.recommended'] = false }
+
+        vim.keymap.set('i', '<cr>', [[pumvisible() ? (complete_info().selected == -1 ? '<c-e><cr>' : '<c-y>') : '<cr>']], { expr = true, noremap = true })
+        vim.keymap.set('i', '<tab>', [[pumvisible() ? '<c-n>' : '<tab>']], { expr = true, noremap = true })
+        vim.keymap.set('i', '<esc>', [[pumvisible() ? '<c-e><esc>' : '<esc>']], { expr = true, noremap = true })
+
+        vim.keymap.set('n', '<leader>cx', ':COQnow -s <cr>', { noremap = true })
+        vim.keymap.set('n', '<leader>cse', ':COQsnips edit <cr>', { noremap = true })
+        vim.keymap.set('n', '<leader>csc', ':COQsnips compile <cr>', { noremap = true })
+    end },
+
+    { 'cohama/lexima.vim', config = function ()
+        vim.fn['lexima#add_rule']({ char = '$', input_after = '$', filetype = 'tex' })
+        vim.fn['lexima#add_rule']({ char = '$', at = [[\%#\$]], leave = 1, filetype = 'tex' })
+        vim.fn['lexima#add_rule']({ char = '<space>', at = [[\%#\$]], input = '<space>', input_after = '<space>', filetype = 'tex' })
+        vim.fn['lexima#add_rule']({ char = '<bs>', at = [[\$\%#\$]], delete = 1, filetype = 'tex' })
+        vim.fn['lexima#add_rule']({ char = '<bs>', at = [[\$\s\%#\s\$]], delete = 1, filetype = 'tex' })
+    end },
+
+    { 'kylechui/nvim-surround', config = function()
+        require("nvim-surround").setup()
+    end },
+
+    { 'folke/zen-mode.nvim', config = function ()
+        require('zen-mode').setup({
+            window = {
+                width = 90,
+                options = {
+                    number = false,
+                    relativenumber = false,
+                }, }
+        })
+
+        vim.keymap.set('n', '<leader>z', ':ZenMode <cr>', { noremap = true })
+    end },
+
 }
 
-require("lazy").setup(plugins)
+require('lazy').setup(plugins)
