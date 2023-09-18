2383b0c3ee4035f90f3da0764d26a999f7ab90f9
John <johnmichaelclasson@gmail.com>
2023-09-19 00:58:34 +1000

    Add .config/nvim/snippets/plaintex.lua Add .config/nvim/snippets/tex.lua

---
+++ dot_config/nvim/snippets/plaintex.lua
@@ -0,0 +1,9 @@
+return {
+    s('document', {
+        t({ '\\documentclass{article}', '\\usepackage{base}', '' }),
+        i(1),
+        t({ '', '\\begin{document}', '' }),
+        i(0),
+        t({ '', '\\end{document}' })
+    })
+}
