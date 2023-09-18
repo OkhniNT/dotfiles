261baf8d749506d1b96415396e084a6bba4a5b3f
John <johnmichaelclasson@gmail.com>
2023-09-19 02:53:08 +1000

    Remove .config/nvim/snippets/plaintex.lua

--- dot_config/nvim/snippets/plaintex.lua
+++
@@ -1,9 +0,0 @@
-return {
-    s('document', {
-        t({ '\\documentclass{article}', '\\usepackage{base}', '' }),
-        i(1),
-        t({ '', '\\begin{document}', '' }),
-        i(0),
-        t({ '', '\\end{document}' })
-    })
-}
