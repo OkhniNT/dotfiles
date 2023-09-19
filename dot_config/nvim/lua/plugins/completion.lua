dcf2cdb6a8ad145e09354de05055ef8550a9bd44
John <johnmichaelclasson@gmail.com>
2023-09-19 19:07:42 +1000

    Update .config/nvim/lua/plugins/completion.lua

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -1,4 +1,10 @@
 return {
+    { 'neovim/nvim-lspconfig', config = function ()
+        local lspconfig = require('lspconfig')
+        lspconfig.clangd.setup {}
+    end },
+    { 'hrsh7th/cmp-nvim-lsp' },
+
     { 'hrsh7th/nvim-cmp', config = function ()
         local cmp = require('cmp')
         cmp.setup({
@@ -10,6 +16,12 @@ return {
             view = {
                 entries = { name = 'custom', selection_order = 'near_cursor' },
             },
+            formatting = {
+                format = function (entry, vim_item)
+                    vim_item.abbr = string.sub(vim_item.abbr, 1, 60)
+                    return vim_item
+                end
+            },
             mapping = cmp.mapping.preset.insert({
                 [ '<c-space>' ] = cmp.mapping.complete(),
                 [ '<c-e>' ] = cmp.mapping.abort(),
@@ -19,13 +31,21 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = 'snippy' },
-                { name = 'buffer' },
+                { name = 'nvim_lsp' },
+                -- { name = 'buffer' },
             }),
+            experimental = { ghost_text = true },
         })
-        vim.keymap.set('n', '<leader>cd', function () cmp.setup.buffer({ enabled = false }) end )
-        vim.keymap.set('n', '<leader>ce', function () cmp.setup.buffer({ enabled = true }) end )
+
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        require('lspconfig')['clangd'].setup {
+            capabilities = capabilities
+        }
+
+        vim.keymap.set('n', '<leader>cd', function () cmp.setup.buffer({ enabled = false }) vim.print('Completion disabled') end)
+        vim.keymap.set('n', '<leader>ce', function () cmp.setup.buffer({ enabled = true }) vim.print('Completion enabled') end)
     end },
-    { 'hrsh7th/cmp-buffer' },
+    -- { 'hrsh7th/cmp-buffer' },
 
     { 'dcampos/nvim-snippy', opts = {
         choice_delay = 500,
