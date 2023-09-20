936beaed1537a3768df738c16b3699544f8dd7cb
John <johnmichaelclasson@gmail.com>
2023-09-21 02:12:37 +1000

    Update .config/awesome/binds/init.lua

--- dot_config/awesome/binds/init.lua
+++ dot_config/awesome/binds/init.lua
@@ -132,12 +132,10 @@ awful.keyboard.append_global_keybindings({
     awful.key({ modkey, "Shift" }, "k", function () awful.client.swap.byidx(-1) end, {description = "swap with previous client by index", group = "client"}),
     awful.key({ modkey }, "l", function () awful.tag.incmwfact( 0.05) end, {description = "increase master width factor", group = "layout"}),
     awful.key({ modkey }, "h", function () awful.tag.incmwfact(-0.05) end, {description = "decrease master width factor", group = "layout"}),
-    awful.key({ modkey, "Shift" }, "h", function () awful.tag.incnmaster(1, nil, true) end, {description = "increase the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Shift" }, "l", function () awful.tag.incnmaster(-1, nil, true) end, {description = "decrease the number of master clients", group = "layout"}),
-    -- awful.key({ modkey, "Control" }, "h", function () awful.tag.incncol( 1, nil, true) end, {description = "increase the number of columns", group = "layout"}),
-    -- awful.key({ modkey, "Control" }, "l", function () awful.tag.incncol(-1, nil, true) end, {description = "decrease the number of columns", group = "layout"}),
-    -- awful.key({ modkey }, "space", function () awful.layout.inc(1) end, {description = "select next", group = "layout"}),
-    -- awful.key({ modkey, "Shift" }, "space", function () awful.layout.inc(-1) end, {description = "select previous", group = "layout"}),
+    awful.key({ modkey, "Shift" }, "comma", function () awful.tag.incnmaster(1, nil, true) end, {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ modkey, "Shift" }, "period", function () awful.tag.incnmaster(-1, nil, true) end, {description = "decrease the number of master clients", group = "layout"}),
+    awful.key({ modkey }, "comma", function () awful.tag.incncol( 1, nil, true) end, {description = "increase the number of columns", group = "layout"}),
+    awful.key({ modkey }, "period", function () awful.tag.incncol(-1, nil, true) end, {description = "decrease the number of columns", group = "layout"}),
 
     -- Focus related keybindings
     awful.key({ modkey }, "j", function () awful.client.focus.byidx( 1) end, {description = "focus next by index", group = "client"}),
@@ -149,6 +147,8 @@ awful.keyboard.append_global_keybindings({
     awful.key({ modkey }, "=", function (s, t) awful.tag.incgap(8, t) awful.layout.arrange(s) end, {description = "increase tag gap size by 8 pixels", group = "screen"}),
     awful.key({ modkey }, "-", function (s, t) awful.tag.incgap(-8, t) awful.layout.arrange(s) end, {description = "increase tag gap size by 8 pixels", group = "screen"}),
     awful.key({ modkey, "Shift" }, "=", function (s, t) awful.tag.setgap(8, t) awful.layout.arrange(s) end, {description = "reset tag gap size to 8 pixels", group = "screen"}),
+    -- Change layout
+    awful.key({ modkey, "Shift" }, "-", function () awful.spawn("layout-select") end, {}),
 
     -- @DOC_NUMBER_KEYBINDINGS@
     awful.key {
