6a384718f98d27081b1e79113d13cbb1c8760b2d
John <johnmichaelclasson@gmail.com>
2023-09-30 01:07:17 +1000

    Add .config/eww/eww.scss Update .config/eww/eww.yuck Update .config/eww/scripts/workspaces-herbstluftwm.sh

--- dot_config/eww/scripts/executable_workspaces-herbstluftwm.sh
+++ dot_config/eww/scripts/executable_workspaces-herbstluftwm.sh
@@ -1,5 +1,26 @@
-herbstclient tag_status | sed 's/\s//g' | sed 's/[0-9]/& /g'
+getworkspaces() {
+    tags="$(herbstclient tag_status | sed 's/\s//g;s/[0-9]/&\n/g;s/\.//g')"
+    echo -n "(box :class 'workspaces'"
+    for tag in $tags; do
+        case $tag in
+            \#*)
+                echo -n "(box :class 'tag_active' '${tag:1}')"
+                ;;
+            -*)
+                echo -n "(box :class 'tag_focused' '${tag:1}')"
+                ;;
+            :*)
+                echo -n "(box :class 'tag_occupied' '${tag:1}')"
+                ;;
+            *)
+                ;;
+        esac
+    done
+    echo -n ")"
+    echo
+}
 
-herbstclient -i focus_changed | while read line; do
-    herbstclient tag_status | sed 's/\s//g' | sed 's/[0-9]/& /g'
+getworkspaces
+{ herbstclient -i tag_changed & herbstclient -i tag_flags & } | while read line; do
+    getworkspaces
 done
