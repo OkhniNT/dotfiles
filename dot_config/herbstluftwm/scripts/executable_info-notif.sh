f3fc2ac5fc6a6766579f372714c7ad832c975a59
John <johnmichaelclasson@gmail.com>
2023-10-04 12:03:03 +1100

    Add .config/herbstluftwm/scripts/info-notif.sh

---
+++ dot_config/herbstluftwm/scripts/executable_info-notif.sh
@@ -0,0 +1,6 @@
+datetime="$(date '+%F %R, %A')"
+
+batcap="$(cat /sys/class/power_supply/BAT0/capacity)"
+battime="$(echo "scale=2; $(cat /sys/class/power_supply/BAT0/charge_now) / $(cat /sys/class/power_supply/BAT0/current_now)" | bc)"
+
+notify-send "Info" "BAT0: $batcap%, $battime\n$datetime"
